35c65174540e6abc39407700c1f66e9f385b780c
Tiago Silva <tiago.silva@contisystems.pt>
2023-03-20 17:41:29 +0000

    add protect pdf program

---
+++ PdfProtector/PDF.cs
@@ -0,0 +1,50 @@
+﻿using iTextSharp.text.pdf;
+using System.Text;
+
+namespace PdfProtector
+{
+    internal class PDF
+    {
+        public static string Protect(string inFolder, string outFolder, string filename, bool closePDF, string ownerPassword)
+        {
+            var source = Path.Combine(inFolder, filename);
+            var destination = Path.Combine(outFolder, filename);
+
+            if (closePDF)
+            {
+                try
+                {
+                    using var reader = new PdfReader(source);
+                    PdfReader.unethicalreading = true;
+                    using var fs = new FileStream(destination, FileMode.Create);
+                    PdfEncryptor.Encrypt(
+                        reader,
+                        fs,
+                        null,
+                        Encoding.UTF8.GetBytes(ownerPassword),
+                        PdfWriter.ALLOW_PRINTING,
+                        PdfWriter.STRENGTH40BITS
+                    );
+                }
+
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                try
+                {
+                    var fi = new FileInfo(source);
+                    fi.MoveTo(destination);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return destination;
+        }
+    }
+}
---
+++ PdfProtector/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using PdfProtector;
+
+var configuration = new ConfigurationBuilder()
+     .AddJsonFile($"appsettings.json");
+
+var config = configuration.Build();
+
+try
+{
+    var inputFolder = config["Inputs:InFolder"]; ;
+    var outputFolder = config["Inputs:OutFolder"]; ;
+    var ownerPassword = config["Inputs:OwnerPassword"];
+    bool isToProtect = bool.Parse(config["Inputs:isToProtect"]);
+
+    var filesPath = Directory.EnumerateFiles(inputFolder, "*.pdf");
+
+    foreach (var path in filesPath)
+    {
+        FileInfo fileInfo = new FileInfo(path);
+        var filename = fileInfo.Name;
+        PDF.Protect(inputFolder, outputFolder, filename, isToProtect, ownerPassword);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw ex;
+}
